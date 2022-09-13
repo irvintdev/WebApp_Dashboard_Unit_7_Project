c18c9dc923a6ae8e105f78c8223a33e0bc19c505
Irvin Torres <irvin7torres7@gmail.com>
2022-09-12 19:33:15 -0700

    Create chart.js

---
+++ web_app_dashboard_v3.2/js/chart.js
@@ -0,0 +1,37 @@
+const mobileCanvas = document.getElementById('mobile-chart');
+
+const mobileData = {
+    labels: [
+        'Desktop',
+        'Tablet',
+        'Phones'
+    ],
+
+    datasets: [{
+        label: 'Mobile Users',
+        data: [300, 125, 125],
+        backgroundColor: [
+            'rgb(116, 120, 186)',
+            'rgb(145, 199, 148)',
+            'rgb(108, 180, 198)'   
+        ],
+        hoverOffset: 4
+    }]
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: {
+        aspectRatio: 1.9,
+        plugins: {
+            legend: {
+                position: 'right',
+                labels: {
+                    boxWidth: 20,
+                    fontWeight: 'bolder',
+                }
+            }
+        }
+    }
+});
