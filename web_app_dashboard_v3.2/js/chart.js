002b9927130b05addabf46d794243207d027d60a
Irvin Torres <irvin7torres7@gmail.com>
2022-09-15 21:35:30 -0700

    Update chart.js

--- web_app_dashboard_v3.2/js/chart.js
+++ web_app_dashboard_v3.2/js/chart.js
@@ -9,6 +9,8 @@ const trafficData = {
     labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
     datasets: [{
         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1900, 2250, 1500, 2500],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
         tension: 0.4
     }]
 };
@@ -16,10 +18,10 @@ const trafficData = {
 const trafficOptions = {
     backgroundColor: 'rgba(112, 104, 201, .5)',
     fill: true,
-    aspectRatio: 1.9,
-    animation: {
-        duration: 0
-    },
+    aspectRatio: 1.99,
+    // animation: {
+    //     duration: 0
+    // },
     scales: {
         y : {
             beginAtZero: true
